fd113027be05e60df90f0858dc93c88c87b9fb3f
xchen774 <68258438+Symorglass@users.noreply.github.com>
2024-07-17 20:34:55 -0400

    modified right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -11,7 +11,7 @@ async function RightSidebar() {
 
   const similarMinds = await fetchUsers({
     userId: user.id,
-    pageSize: 1,
+    pageSize: 20,
   });
 
   const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
