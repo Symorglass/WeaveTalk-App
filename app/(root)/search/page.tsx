755062d99e379058eaef25a174432f8b866bfc4b
xchen774 <68258438+Symorglass@users.noreply.github.com>
2024-07-17 20:28:50 -0400

    added user search feature

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,61 +1,66 @@
+import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
-import { redirect } from 'next/navigation';
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
 import UserCard from "@/components/cards/UserCard";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import Image from "next/image";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { profileTabs } from "@/constants";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
-import ThreadsTab from "@/components/shared/ThreadsTab";
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
 
+  const user = await currentUser();
+  if (!user) return null;
 
-async function Page() {   
-    
-    const user = await currentUser();
-    if(!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-    // check if user is onboarded
-    const userInfo = await fetchUser(user.id);    
-    if(!userInfo?.onboarded) redirect('/onboarding');  
-
-    // Fetch all searched users 
-    const result = await fetchUsers({
-        userId: user.id,
-        searchString: '',
-        pageNumber: 1,
-        pageSize: 25
-    });
-
+  // fetch user using the searched words in searchParams
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
   return (
     <section>
-        <h1 className="head-text mb-10">Search</h1>
+      <h1 className='head-text mb-10'>Search</h1>
 
-        {/* TODO: Search bar */}
+      <Searchbar routeType='search' />
 
-        <div className="mt-14 flex flex-col gap-9">
-            {result.fetchedUsers.length === 0 ? (
-                <p className="no-result">No user found</p>
-            ) : (
-                <>
-                    {result.fetchedUsers.map((person) => (
-                    <UserCard
-                        key={person.id}
-                        id={person.id}
-                        name={person.name}
-                        username={person.username}
-                        imgUrl={person.image}
-                        personType='User'
-                    />
-                    ))}
-                </>
-            )}
-        </div>
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.fetchedUsers.length === 0 ? (
+            <p className='no-result'>
+                No Result
+            </p>
+        ) : (
+          <>
+            {result.fetchedUsers.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType='User'
+              />
+            ))}
+          </>
+        )}
+      </div>
 
+      <Pagination
+        path='search'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
-  )
+  );
 }
 
 export default Page;
